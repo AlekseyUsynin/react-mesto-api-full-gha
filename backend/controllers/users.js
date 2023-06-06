9bdad33ffa2c566a71b545746d4693568f93ef53
Aleksey Usynin <fesskagf@yandex.ru>
2023-06-06 19:10:16 +0400

    fix token

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -18,7 +18,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000,
           httpOnly: true,
           sameSite: true,
-        }).send({ token });
+        }).send(user.toJSON());
     })
     .catch((err) => {
       next(err);
