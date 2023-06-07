e00601ffde1dff5dbbd934bd5793c7746c019359
Aleksey Usynin <fesskagf@yandex.ru>
2023-06-07 17:15:58 +0400

    fix

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -6,6 +6,7 @@ const checkResponse = (res) =>
 export const register = (email, password) => {
   return fetch(`${baseUrl}/signup`, {
     method: "POST",
+    credentials: 'include',
     headers: {
       "Content-Type": "application/json",
       'Accept': 'application/json',
@@ -17,7 +18,7 @@ export const register = (email, password) => {
 export const authorize = (email, password) => {
   return fetch(`${baseUrl}/signin`, {
     method: "POST",
-    credentials: 'include',
+    // credentials: 'include',
     headers: {
       "Content-Type": "application/json",
       'Accept': 'application/json',
